c6f45fb91e8c54be82e60e484e9ebe55f7f3f462
ykl7 <yklal95@gmail.com>
2016-12-15 12:43:20 +0530

    sentiment docs and remove useless log statements

---
+++ SENTIMENT_DOCUMENTATION.md
@@ -0,0 +1,20 @@
+# Sentiment Classifier
+
+## Command sequence to run
+```shell
+python stem.py
+python tokens.py
+python sentiment.py
+python classify.py
+```
+
+## What it does
+* Uses PorterStemmer to find stems of all tokenize
+* Tokenize input data set
+* Build Logistic Regression model, evaluate, save and use it
+
+### Special Thanks
+* [scikit-learn](http://scikit-learn.org/stable/)
+* [nltk](http://www.nltk.org)
+* [pandas](http://pandas.pydata.org)
+* [numpy](http://www.numpy.org)
--- app/sentiment_analysis/sentiment.py
+++ app/sentiment_analysis/sentiment.py
@@ -18,16 +18,6 @@ test_df.columns = ["Text"]
 train_df = pd.read_csv(train_file, header=None, delimiter="\t", quoting=3)
 train_df.columns = ["Sentiment","Text"]
 
-print train_df.shape
-print test_df.shape
-
-print train_df.head()
-print test_df.head()
-
-print train_df.Sentiment.value_counts()
-
-print np.mean([len(s.split(" ")) for s in train_df.Text])
-
 vectorizer = CountVectorizer(
     analyzer = 'word',
     tokenizer = tokens.tokenize,
@@ -39,14 +29,10 @@ vectorizer = CountVectorizer(
 corpus_data_features = vectorizer.fit_transform(train_df.Text.tolist() + test_df.Text.tolist())
 
 corpus_data_features_nd = corpus_data_features.toarray()
-print corpus_data_features_nd.shape
 
 vocab = vectorizer.get_feature_names()
-print vocab
 
 dist = np.sum(corpus_data_features_nd, axis=0)
-for tag, count in zip(vocab, dist):
-    print count, tag
 
 X_train, X_test, y_train, y_test  = train_test_split(
     corpus_data_features_nd[0:len(train_df)], 
@@ -59,7 +45,7 @@ log_model = log_model.fit(X=X_train, y=y_train)
 
 y_pred = log_model.predict(X_test)
 
-print(classification_report(y_test, y_pred))
+print (classification_report(y_test, y_pred))
 
 log_model = LogisticRegression()
 log_model = log_model.fit(X=corpus_data_features_nd[0:len(train_df)], y=train_df.Sentiment)
